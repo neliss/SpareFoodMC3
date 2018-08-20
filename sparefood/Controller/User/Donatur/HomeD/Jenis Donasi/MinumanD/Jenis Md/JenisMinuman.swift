9d0d4181d895505d246d603c1a92be4d13a612f6
muhammad sutrisno <msutrisno86@gmail.com>
2018-08-20 22:15:32 +0700

    - tris controller jenis2 donasi

---
+++ sparefood/Controller/User/Donatur/HomeD/Jenis Donasi/MinumanD/Jenis Md/JenisMinuman.swift
@@ -0,0 +1,35 @@
+//
+//  JenisMinuman.swift
+//  sparefood
+//
+//  Created by muhammad sutrisno on 20/08/18.
+//  Copyright © 2018 Nelis Lasta. All rights reserved.
+//
+
+import UIKit
+
+class JenisMinuman: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
