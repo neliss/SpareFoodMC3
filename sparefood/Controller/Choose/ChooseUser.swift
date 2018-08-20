702b6863a07190728a3a57eeff13fec24720574b
muhammad sutrisno <msutrisno86@gmail.com>
2018-08-20 17:13:53 +0700

    - tris Controller

---
+++ sparefood/Controller/Choose/ChooseUser.swift
@@ -0,0 +1,47 @@
+//
+//  ChooseUser.swift
+//  sparefood
+//
+//  Created by muhammad sutrisno on 20/08/18.
+//  Copyright © 2018 Nelis Lasta. All rights reserved.
+//
+
+import UIKit
+
+class ChooseUser: UIViewController {
+
+    
+    @IBOutlet weak var lblBtnDonatur: UIButton!
+    
+    @IBOutlet weak var lblBtnKomunitas: UIButton!
+    
+    @IBAction func btnDonatur(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnKomunitas(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
